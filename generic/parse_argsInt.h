bc27b74dfc4c164c5ba92146dfb19dd77bc03624
Thomas Perschak <social@teclab.at>
2026-02-03 10:41:03 +0100

    prepare for Tcl90

--- generic/parse_argsInt.h
+++ generic/parse_argsInt.h
@@ -6,6 +6,16 @@
 #endif
 
 #include <tcl.h>
+
+#ifndef TCL_SIZE_MAX
+/* Tcl 8 compatibility */
+#include <limits.h>
+typedef int Tcl_Size;
+#define TCL_SIZE_MAX INT_MAX
+#define TCL_SIZE_MODIFIER ""
+#define Tcl_GetSizeIntFromObj Tcl_GetIntFromObj
+#endif
+
 #include "tclstuff.h"
 #include "tip445.h"
 #include <string.h>
